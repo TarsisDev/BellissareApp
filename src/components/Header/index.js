8206aad8701f243cc904599d084be592c2e32750
Laionhora <132407468+Laionhora@users.noreply.github.com>
2024-11-17 01:52:51 -0300

    Alteação no texto do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,11 +6,11 @@ export default function Header() {
   return (
     <Container>
       <Text size={24} color="white" >
-        ToDo
+        Bellissare
         <Text size={24} weight="700" color="white">APP</Text>
       </Text>
 
-      <Text opacity={0.5} color="white">Add, delete ou marque as tarefas executadas</Text>
+      <Text opacity={0.5} color="white">Marque já a sua sessão.</Text>
     </Container>
   );
 }
