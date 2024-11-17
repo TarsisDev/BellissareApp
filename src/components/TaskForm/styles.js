e3c7c5cdf8ebe3dea6d6557c03a202d6187a0e59
TarsisDev <tarsisns.code@gmail.com>
2024-11-17 01:07:37 -0300

    borda corrigida em TaskForm

--- src/components/TaskForm/styles.js
+++ src/components/TaskForm/styles.js
@@ -6,7 +6,7 @@ export const Form = styled.View`
 `;
 
 export const Input = styled.TextInput`
-  border: 1px solid rgba(204, 204, 204, 0.9);
+  border: 1px solid rgba(0, 0, 0, 0.9);
   padding: 16px;
   border-radius: 8px;
 `;
