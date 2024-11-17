db54eececc8bb36c0420a29fdd357fb779286dad
Laionhora <132407468+Laionhora@users.noreply.github.com>
2024-11-17 01:21:34 -0300

    Alteação na cor do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,12 +5,12 @@ import { Container } from './styles';
 export default function Header() {
   return (
     <Container>
-      <Text size={24}>
+      <Text size={24} color="white" >
         ToDo
-        <Text size={24} weight="700">APP</Text>
+        <Text size={24} weight="700" color="white">APP</Text>
       </Text>
 
-      <Text opacity={0.5}>Add, delete ou marque as tarefas executadas</Text>
+      <Text opacity={0.5} color="white">Add, delete ou marque as tarefas executadas</Text>
     </Container>
   );
 }
